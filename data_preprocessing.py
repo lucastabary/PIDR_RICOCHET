6d8e542001f18adc2db7e3c236816250750850bf
Lucas <lucas.tabary01@gmail.com>
2026-02-17 18:32:41 +0100

    - dates standardisation - Dates_Delta

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,6 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+def standardize_dates(df):
+    df["Date_Floraison"] = pd.to_datetime(df["Date_Floraison"], format="%d/%m/%Y")
+
+    df['Date_SPIR'] = df['Date_SPIR'].str.replace('juin', '06', case=False)
+    df['Date_SPIR'] = df['Date_SPIR'].str.replace('mai', '05', case=False)
+    df['Date_SPIR'] = df['Date_SPIR'].str.replace('nov', '11', case=False)
+    df["Date_SPIR"] = pd.to_datetime(df["Date_SPIR"], format="%d-%m-%y")
+    return df
+
+def add_days_to_floraison(df):
+    df["Dates_Delta"] = (df["Date_SPIR"] - df["Date_Floraison"]).dt.days
+    return df
+
 data = pd.read_csv("./Data/CIRAD_Sorghum2023_SPIR_all_2 (4).csv",sep=";")
 
 Control_plantes = data.loc[data["Traitement"]=="RS1",:]
@@ -10,4 +23,4 @@ Control_plantes = Control_plantes.drop_duplicates("ID")
 print(Control_plantes)
 
 print(Control_plantes["ID"].describe())
-
+print()
