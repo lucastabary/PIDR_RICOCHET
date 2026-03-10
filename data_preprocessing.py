e4f2b13e0c1c2e0854d7d54995db96e276599932
Lucas <lucas.tabary01@gmail.com>
2026-03-10 14:13:15 +0100

    group separation

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -18,6 +18,18 @@ def add_days_to_floraison(df):
     df["Dates_Delta"] = (df["Date_SPIR"] - df["Date_Floraison"]).dt.days
     return df
 
+def split_data(df):
+    """
+    Retourne un dictionnaire de sous-dataframes indexé par (Genotype, Sample).
+    Ex: groups[("Btx623", "FreshLeaf")], groups[("Macia", "GrainFlour")], ...
+    """
+    groups = {}
+    for genotype in df["Genotype"].unique():
+        for sample in df["Sample"].unique():
+            mask = (df["Genotype"] == genotype) & (df["Sample"] == sample)
+            groups[(genotype, sample)] = df.loc[mask, :]
+    return groups
+
 if __name__ == "__main__":
     data = pd.read_csv("./Data/CIRAD_Sorghum2023_SPIR_all_2 (4).csv",sep=";")
 
